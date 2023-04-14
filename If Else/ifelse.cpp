14ad3cdc92d6e5fa60d068c38d2f08005a559566
Aashay Agrawal <aashay.aa12@gmail.com>
2023-04-14 13:23:38 +0530

    added if else

---
+++ If Else/ifelse.cpp
@@ -0,0 +1,20 @@
+// If  Else
+#include <iostream>
+using namespace std;
+int main()
+{
+  /* int n;
+   cin >> n;
+   cout << "value of n is :" << n <<endl;
+ */
+
+  int a;
+  if (a > 0)
+  {
+    cout << "A is Positive" << endl;
+  }
+  else
+  {
+    cout << "A is negative" << endl;
+  }
+}
