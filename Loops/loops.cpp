4f218ec8e3164ace237c5d281f31a211ee26da61
Aashay Agrawal <aashay.aa12@gmail.com>
2023-04-14 16:55:25 +0530

    Added Loops

---
+++ Loops/loops.cpp
@@ -0,0 +1,30 @@
+// Loops
+#include <iostream>
+using namespace std;
+int main()
+{
+  /* int n;
+   cin >> n;
+   int i = 1;
+   while (i <= n)
+   {
+     cout << i << "";
+     i = i + 1;
+   }
+   for printing 1 to n
+   */
+ int n;
+  cin >> n;
+  int i = 1;
+  int sum = 0;
+  while (i <= n)
+  {
+    sum = sum + i;
+    i = i + 1;
+  }
+  cout << " value of sum is " << sum << endl;
+} 
+
+
+
+
