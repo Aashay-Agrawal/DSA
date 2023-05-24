531aa202886fea8acf2efad9ca660cec5bab0a71
Aashay Agrawal <aashay.aa12@gmail.com>
2023-05-24 18:13:34 +0530

    Added Array

---
+++ Arrays/af.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int size;
+cin >> size;
+int num[100];
+for(int i = 0; i<size; i++)
+{
+  cin >> num[i];
+  
+}
+
+  return 0;
+}
---
+++ Arrays/arrays.cpp
@@ -0,0 +1,14 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+int car[5] = {1, 2, 3};
+cout << car[2] << endl;
+
+return 0;
+
+}
+ 
