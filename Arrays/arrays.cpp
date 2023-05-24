6906ed70dc12ae6c1e79b2ca4bc691b0bd666fcc
Aashay Agrawal <aashay.aa12@gmail.com>
2023-05-24 18:48:58 +0530

    added reverse

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -1,14 +1,12 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 
 {
 
-int car[5] = {1, 2, 3};
-cout << car[2] << endl;
+  int car[5] = {1, 2, 3};
+  cout << car[2] << endl;
 
-return 0;
-
+  return 0;
 }
- 
