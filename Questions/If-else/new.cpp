af59e59a2662cd3b6aa77270f674db4627cc4190
Aashay Agrawal <aashay.aa12@gmail.com>
2023-08-19 13:29:14 +0530

    if

---
+++ Questions/If-else/new.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int a, b;
+
+  cin >> a >> b;
+
+  if (a >= b)
+  {
+    cout << "greater";
+  }
+  else
+  {
+    cout << "lesser";
+  }
+  return 0;
+}
