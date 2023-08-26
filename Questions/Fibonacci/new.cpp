3ece670565e3ce1d5c3688c0a29e7a13fbc8a57d
Aashay Agrawal <aashay.aa12@gmail.com>
2023-08-26 11:33:00 +0530

    new ques

---
+++ Questions/Fibonacci/new.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int fubo()
+{
+  // nahi ho raha ye question
+}
---
+++ Questions/Sum of Array/new.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout << "enter the number" << endl;
+cin >> n;
+
+// not able to get the logic
+
+// is there any way to convert number into array then sum the elements of the array??
+
+
+
+}
